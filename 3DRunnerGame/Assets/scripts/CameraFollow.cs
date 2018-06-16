9784a1f6e9c135ba053e4f45b2d38bdfa7d74b06
Vishal Singh <svishal910@gmail.com>
2018-06-16 22:22:37 +0530

    initial commit

---
+++ 3DRunnerGame/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField]
+	Transform player;
+	Vector3 offset;
+
+	[SerializeField]
+	float smoothRate;
+
+
+	// Use this for initialization
+	void Start () {
+		offset = player.position - this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		Vector3 currPos = transform.position;
+		Vector3 newPos = player.position - offset;
+		currPos = Vector3.Lerp (currPos, newPos, smoothRate*Time.deltaTime);
+		transform.position = currPos;
+
+	}
+}
